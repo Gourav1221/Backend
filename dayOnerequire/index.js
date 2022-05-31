dd62dfb480696271a40dd2e26d1e891675d25bbb
Gourav1221 <90262724+Gourav1221@users.noreply.github.com>
2022-06-01 01:28:53 +0530

    os

--- dayOnerequire/index.js
+++ dayOnerequire/index.js
@@ -10,13 +10,15 @@
 
 // console.log(x.addfive(5))
 
-
+const os = require("os")
 const fs = require("fs")
 const path = require("path")
 
-const data = fs.readFileSync(path.join("./name.txt"),{encoding:"utf8"})
-console.log('data:', data)
+// const data = fs.readFileSync(path.join("./name.txt"),{encoding:"utf8"})
+// console.log('data:', data)
 
+
+console.log(os.cpus().map((e)=>e.speed))
 
 //  fs.readFile("./name.txt",{encoding:"utf8"},(err,data) => {
 //      console.log(data)
