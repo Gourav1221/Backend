f98b6a72a6be5ffd1e082070b4411a858719d2da
Gourav1221 <90262724+Gourav1221@users.noreply.github.com>
2022-06-28 22:52:40 +0530

    learn express

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,35 +1,7 @@
-
-const { default: axios } = require('axios');
 const express = require('express');
 
 const app = express();
 
-// app.use(express.urlencoded)
-app.use(express.json())
-
-
-
-app.get("/",  (req, res) => {
-
-   axios.get("http://localhost:8080/product").then(({data}) => {
-    //    console.log(data)
-       res.setHeader('Content-Type', 'application/json')
-       return res.status(200).send(data)
-   })
-    
-})
-
-// app.get("/product",(req, res) => {
-//     res.send(JSON.stringify({a:"gourav",b:"mishra"}))
-// })
-
-app.post("/product",(req, res) => {
-    axios.post("http://localhost:8080/product",req.body)
-    console.log(req.body)
-    return res.status(200).send("got data")
-})
-
-
 app.listen(8080,()=>{
-    console.log("listen on 8080")
+    res.send("listen on 8080");
 })
