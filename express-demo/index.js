787f9c11cec0cf694e86e3b839809da330a6eb4b
Gourav1221 <90262724+Gourav1221@users.noreply.github.com>
2022-07-02 20:13:10 +0530

    complete

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,5 +1,6 @@
 
 const express = require('express')
+const fs = require('fs')
 
 
 const app = express()
@@ -11,17 +12,77 @@ app.get('/', (req, res) => {
     res.send("world")
 })
 
+// MULTIPLE ROUTE
+
+// app.get(['/',"index"], (req, res) => {
+    
+//     res.send("world")
+// })
+
+
+
+
+
+
 app.get("/product",(req, res) => {
-    res.send({a:"abc",b:"ab"})
+
+    fs.readFile("db.json",{encoding: "utf8"},(err,data)=>{
+        res.send(data)
+    })
+   
 })
 
 app.post("/product",(req, res) => {
-    console.log(req.body)
-    res.send("created successfully")
+
+    fs.readFile("./db.json",{encoding:"utf8"},(err,data)=>{
+        const parsed = JSON.parse(data)
+        parsed.product = [...parsed.product,req.body]
+
+
+        fs.writeFile("./db.json",JSON.stringify(parsed),{encoding:"utf8"},(err,data)=>{
+            res.send("product created")
+        })
+
+    })
+
+   // res.send("created successfully")
 
 })
 
+app.put("/product/:id",(req,res)=>{
+  
+    fs.readFile("db.json","utf-8",(err,data)=>{
+        let parsed = JSON.parse(data)
 
+        parsed.product =  parsed.product.filter((e)=>{
+        return e.id != req.params.id
+       })
+
+       parsed.product = [...parsed.product,req.body]
+
+       fs.writeFile("./db.json",JSON.stringify(parsed),{encoding:"utf8"},(err,data)=>{
+        res.send("update successfully")
+       })
+
+
+    
+    })
+})
+app.delete("/product/:id",(req,res)=>{
+    
+    fs.readFile("db.json","utf-8",(err,data)=>{
+        let parsed = JSON.parse(data)
+        parsed.product = parsed.product.filter((e)=>e.id != req.params.id)
+
+        fs.writeFile("db.json",JSON.stringify(parsed),"utf-8",(err,data)=>{
+            res.send("product deleted")
+        })
+    })
+})
+
+app.all("*",(req, res)=>{
+    res.status(404).send("not found")
+})
 
 app.listen(8000,()=>{
     console.log("started")
